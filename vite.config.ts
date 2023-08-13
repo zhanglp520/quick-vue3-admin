24dced5d50bb97feaee36659daa3552de81f34ce
土豆哥 <zhanglp15229380174@163.com>
2023-08-13 18:55:02 +0800

    配置格式化插件

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,18 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin({
-      include: ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  server: {
-    host: "0.0.0.0",
-  },
+	plugins: [
+		vue(),
+		eslintPlugin({
+			include: ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"],
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
+	server: {
+		host: "0.0.0.0",
+	},
 });
