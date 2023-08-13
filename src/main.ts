cdcac1f768d4bd44fcab9516a63668ce9327c0ce
土豆哥 <zhanglp15229380174@163.com>
2023-08-13 19:39:53 +0800

    安装element-plus; 安装quick-vue3-ui;

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import QuickVue3Ui from "@ainiteam/quick-vue3-ui";
+import "@ainiteam/quick-vue3-ui/dist/style.css";
 import "./style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
 app.use(ElementPlus);
+app.use(QuickVue3Ui);
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,6 @@ export default defineConfig({
 	},
 	server: {
 		host: "0.0.0.0",
+		port: 5173,
 	},
 });
